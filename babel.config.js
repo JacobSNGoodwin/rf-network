8579e4a6021ced5b362cad78dd197494481dd1db
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-05 22:37:19 -0700

    Babel config for jest

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// babel.config.js - for testing purposes
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
+}
