c26d2d1818f096e752d38d2ff20d8b53d50f5e42
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-11 17:03:52 -0700

    Opt for class to take in string

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,32 @@
-// import { OptionsValues } from './interfaces'
-
-class Network {
-  static touchStoneToText(event: Event): Promise<string> {
-    // check if file exists and create, otherwise throw an error
-    const eventTarget = <HTMLInputElement>event.target
-    const fileList = eventTarget.files
-
-    if (!fileList) {
-      throw new Error('File does not exist on input')
-    }
-
-    const file = fileList[0]
-
-    // setup file reader
-    const reader = new FileReader()
-    reader.readAsText(file)
-
-    // get file type and number of ports to verify proper data formate
-    const fileType = file.name.split('.').pop()
-    if (!fileType) {
-      throw new Error('File extension could not be extracted')
-    }
-    const digitMatch = fileType.match(/\d+/g)
-    if (!digitMatch) {
-      throw new Error('Could not extract number of ports from file extension')
-    }
-    const nPorts: number = +digitMatch[0]
-
-    return new Promise<string>((resolve, reject) => {
-      reader.onload = () => {
-        // since our file reader reads as text, we expect a string isntead of arrayBuffer
-        resolve(<string>reader.result)
-      }
-      reader.onerror = () => {
-        reject('Failed to load file')
-      }
-    })
-  }
+interface TouchstoneData {
+  // freqUnit: string
+  // paramType: string
+  // format: string
+  Z0: number
 }
 
-// const loadSparams = (event: Event): Promise<OptionsValues> => {
-//   // check if file exists and create, otherwise throw an error
-//   const eventTarget = <HTMLInputElement>event.target
-//   const fileList = eventTarget.files
+class Network {
+  private _touchstoneText: string
+  private _touchstoneData: TouchstoneData
 
-//   if (!fileList) {
-//     throw new Error('File does not exist on input')
-//   }
+  get touchstoneText() {
+    return this._touchstoneText
+  }
 
-//   const file = fileList[0]
+  get refImpedance() {
+    return this._touchstoneData.Z0
+  }
 
-//   // setup file reader
-//   const reader = new FileReader()
-//   reader.readAsText(file)
+  constructor(touchstoneText: string) {
+    this._touchstoneText = touchstoneText
+    this._touchstoneData = this.parseTouchstoneText(touchstoneText)
+  }
 
-//   // get file type and number of ports to verify proper data formate
-//   const fileType = file.name.split('.').pop()
-//   if (!fileType) {
-//     throw new Error('File extension could not be extracted')
-//   }
-//   const digitMatch = fileType.match(/\d+/g)
-//   if (!digitMatch) {
-//     throw new Error('Could not extract number of ports from file extension')
-//   }
-//   const nPorts: number = +digitMatch[0]
-
-//   return new Promise<OptionsValues>((resolve, reject) => {
-//     reader.onload = () => {
-//       // since our file reader reads as text, we expect a string isntead of arrayBuffer
-//       const fileText = <string>reader.result
-//       console.log(fileText)
-//       const textArray = fileText.split('\n')
-
-//       let optionsIndex: number | null = null // index of options line beginning with #
-//       let dataIndex: number | null = null // start index of actual data
-
-//       for (let i = 0; i < textArray.length; i++) {
-//         if (textArray[i][0] === '#') {
-//           optionsIndex = i
-//         }
-//         if (
-//           optionsIndex && // we've already found options
-//           textArray[i] && // the line has text
-//           i > optionsIndex && // we're past the options index
-//           textArray[i][0] !== '!' // it's not a comment line (can be comment after options, eww)
-//         ) {
-//           // start of data which needs to be handled different for different
-//           // values of nPort
-//           dataIndex = i
-//           break
-//         }
-//       }
-
-//       // parse options into desired formats
-//       if (!optionsIndex) {
-//         throw new Error('Could not find an options line in file')
-//       }
-//       const options = parseOptions(textArray[optionsIndex])
-//       resolve(options)
-//     }
-//   })
-// }
-
-// const parseOptions = (optionsString: string): OptionsValues => {
-//   const options = {
-//     freqUnit: 'GHz',
-//     paramType: 'S',
-//     format: 'MA',
-//     Z0: 50
-//   }
-//   return options
-// }
+  private parseTouchstoneText(text: string): TouchstoneData {
+    return {
+      Z0: 50
+    }
+  }
+}
 
 export { Network as default }
--- src/interfaces.ts
+++
@@ -1,8 +0,0 @@
-interface OptionsValues {
-  freqUnit: string
-  paramType: string
-  format: string
-  Z0: number
-}
-
-export { OptionsValues }
