dc5754fe912b3210ef321ffc99a2eea41b2b5bb3
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-11 19:22:26 -0700

    Load touchstone files as strings

--- tests/options.test.js
+++ tests/options.test.js
@@ -1,20 +1,27 @@
 import path from 'path'
+
+import { Network } from '../dist/index'
 import readFileAsync from './util/readFileAsync'
 
 let s1p_1, s1p_2, s2p_1, s2p_2, s2p_3, s3p_1, s4p_1
 
 beforeAll(async () => {
   s1p_1 = await readFileAsync(path.join(__dirname, 'testFiles', 'example1.s1p'))
-  s1p_2 = await readFileAsync(path.join(__dirname, 'testFiles', 'example2.s1p'))
+  // s1p_2 = await readFileAsync(path.join(__dirname, 'testFiles', 'example2.s1p'))
   s2p_1 = await readFileAsync(path.join(__dirname, 'testFiles', 'example1.s2p'))
-  s2p_2 = await readFileAsync(path.join(__dirname, 'testFiles', 'example2.s2p'))
-  s2p_3 = await readFileAsync(path.join(__dirname, 'testFiles', 'example3.s2p'))
+  // s2p_2 = await readFileAsync(path.join(__dirname, 'testFiles', 'example2.s2p'))
+  // s2p_3 = await readFileAsync(path.join(__dirname, 'testFiles', 'example3.s2p'))
   s3p_1 = await readFileAsync(path.join(__dirname, 'testFiles', 'example1.s3p'))
   s4p_1 = await readFileAsync(path.join(__dirname, 'testFiles', 'example1.s4p'))
 })
 
 describe('Properly loads options for example files', () => {
-  test('loads a file', () => {
-    expect(s1p_1).toBeTruthy()
+  test('s1p file', () => {
+    const network = new Network(s1p_1)
+    expexct(true).toBeTruthy()
+    // expect(network.options.freqUnit).toBe('GHz')
+    // expect(network.options.paramType).toBe('S')
+    // expect(network.options.format).toBe('MA')
+    // expect(network.options.z0).toBe(50)
   })
 })
