580359b6080bdbcc7f29ff6edfa13eaf0ee01ab6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-11 18:53:54 -0700

    Read in files as utf-8

--- tests/util/readFileAsync.js
+++ tests/util/readFileAsync.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 const readFileAsync = fileName => {
   return new Promise(function(resolve, reject) {
-    fs.readFile(fileName, (err, data) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
       err ? reject(err) : resolve(data)
     })
   })
