5321688dab6faba19630f29d785560aa8203c6bc
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-30 20:06:44 -0700

    Function for all Sparams types at freq

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,29 +7,35 @@ interface Options {
 
 interface NetworkData {
   freq: number[]
-  s: SData[][][]
+  s: SData[][]
 }
 
 interface AllS {
-  sRe: number[]
-  sIm: number[]
-  sMag: number[]
-  sDb: number[]
-  sAngle: number[]
-  sDeg: number[]
+  sRe: number
+  sIm: number
+  sMag: number
+  sDb: number
+  sAngle: number
+  sDeg: number
 }
 
-interface SData extends AllS {
+interface SData {
+  sRe: number[]
   sReMin: number
   sReMax: number
+  sIm: number[]
   sImMin: number
   sImMax: number
+  sMag: number[]
   sMagMin: number
   sMagMax: number
+  sDb: number[]
   sDbMin: number
   sDbMax: number
+  sAngle: number[]
   sAngleMin: number
   sAngleMax: number
+  sDeg: number[]
   sDegMin: number
   sDegMax: number
 }
--- src/util/dataFromTextLines.ts
+++ src/util/dataFromTextLines.ts
@@ -23,25 +23,46 @@ export default function dataFromTextLines(
   // initialize the data array
   for (let i = 0; i < data.s.length; i++) {
     data.s[i] = new Array(nPorts)
-    for (let j = 0; j < data.s[i].length; j++) {
-      data.s[i][j] = new Array()
-    }
   }
 
   // split by any number of white space
   const splitter = new RegExp('\\s+')
 
-  // create a function to map data to complex dataType of real and imaginary
-  const toComplex = (term1: number, term2: number): math.Complex => {
+  // create a function to get all Sparameter types from a single
+  // type described in the touchstone file
+  const toAllS = (term1: number, term2: number): AllS => {
     if (options.importFormat === 'MA') {
       const angle = (term2 * Math.PI) / 180
-      return math.complex(<math.PolarCoordinates>{ r: term1, phi: angle })
+      return {
+        sRe: term1 * math.cos(angle),
+        sIm: term1 * math.sin(angle),
+        sMag: term1,
+        sDb: 20 * math.log10(term1),
+        sAngle: angle,
+        sDeg: term2
+      }
     } else if (options.importFormat === 'DB') {
       const linMag = Math.pow(10, term1 / 20)
       const angle = (term2 * Math.PI) / 180
-      return math.complex(<math.PolarCoordinates>{ r: linMag, phi: angle })
+      return {
+        sRe: linMag * math.cos(angle),
+        sIm: linMag * math.sin(angle),
+        sMag: linMag,
+        sDb: term1,
+        sAngle: angle,
+        sDeg: term2
+      }
     } else if (options.importFormat === 'RI') {
-      return math.complex(<math.Complex>{ re: term1, im: term2 })
+      const linMag = math.sqrt(term1 * term1 + term2 * term2)
+      const angle = math.atan2(term2, term1)
+      return {
+        sRe: term1,
+        sIm: term2,
+        sMag: linMag,
+        sDb: 20 * math.log10(linMag),
+        sAngle: angle,
+        sDeg: (angle * 180) / Math.PI
+      }
     } else {
       throw new Error('Unknown data format type.')
     }
@@ -66,21 +87,22 @@ export default function dataFromTextLines(
     // let s = <math.Matrix>math.zeros(nPorts, nPorts)
     // remember that for n = 2, the its [[S11, S21], [S12, S22]]
     if (nPorts === 1) {
-      data.s[0][0].push(toComplex(+singleFreq[0], +singleFreq[1]))
+      const sAtFreq = toAllS(+singleFreq[0], +singleFreq[1])
+      // data.s[0][0].push(toAllS(+singleFreq[0], +singleFreq[1]))
     } else if (nPorts === 2) {
-      data.s[0][0].push(toComplex(+singleFreq[0], +singleFreq[1]))
-      data.s[0][1].push(toComplex(+singleFreq[4], +singleFreq[5]))
-      data.s[1][0].push(toComplex(+singleFreq[2], +singleFreq[3]))
-      data.s[1][1].push(toComplex(+singleFreq[6], +singleFreq[7]))
+      // data.s[0][0].push(toAllS(+singleFreq[0], +singleFreq[1]))
+      // data.s[0][1].push(toAllS(+singleFreq[4], +singleFreq[5]))
+      // data.s[1][0].push(toAllS(+singleFreq[2], +singleFreq[3]))
+      // data.s[1][1].push(toAlls(+singleFreq[6], +singleFreq[7]))
     } else {
       for (let i = 0; i < nPorts; i++) {
         for (let j = 0; j < nPorts; j++) {
-          data.s[i][j].push(
-            toComplex(
-              +singleFreq[2 * (3 * i + j)],
-              +singleFreq[2 * (3 * i + j) + 1]
-            )
-          )
+          // data.s[i][j].push(
+          //   toAllS(
+          //     +singleFreq[2 * (3 * i + j)],
+          //     +singleFreq[2 * (3 * i + j) + 1]
+          //   )
+          // )
         }
       }
     }
